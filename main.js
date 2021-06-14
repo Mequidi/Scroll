13891774b66f59a22efab6645ee2a1c708ec974a
Mequidi <nikhilsejwal234@gmail.com>
2021-06-15 00:01:01 +0530

    fixed nav bar and top bnt

--- main.js
+++ main.js
@@ -1,6 +1,49 @@
+const date = document.getElementById("date");
+date.textContent = new Date().getFullYear();
+
+
 const toggleBtn = document.querySelector(".toggle-btn");
 const links = document.querySelector(".links");
+const nav = document.querySelector(".nav");
+const allLinks = document.querySelectorAll(".link-item");
+const topBtn = document.querySelector(".top-btn");
+let containerHeight = 0;
+
 
 toggleBtn.addEventListener("click",function(){
-    links.classList.toggle("show-links");
+    // links.classList.toggle("show-links");
+    allLinks.forEach(function(item){
+        containerHeight += item.getBoundingClientRect().height;
+    })
+    if(links.getBoundingClientRect().height === 0)
+    { 
+        links.style.height = `${containerHeight}px`;
+        links.classList.add("show-links");
+    }
+    else
+    {    
+        links.style.height = `0px`;
+        containerHeight = 0;
+        links.classList.remove("show-links");
+    } 
 })
+
+window.addEventListener("scroll",function(){
+    const scrollHeight = window.pageYOffset;
+    const navHeight = nav.getBoundingClientRect().height;
+    if(scrollHeight > navHeight)
+        nav.classList.add("fixed-nav");
+    else    
+        nav.classList.remove("fixed-nav");
+    if(scrollHeight > 500)  
+    {    
+        topBtn.classList.add("show-links")
+    }
+    else
+        topBtn.classList.remove("show-links");
+})  
+
+// allLinks.forEach(function(link){
+//     consol.log(link.getBoundingClientRect());
+// })
+
